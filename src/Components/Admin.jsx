ac558de159274d1bbec93f36a4151e117b8a6daa
PasiduSupunsara <k.a.p.supunsara@gmail.com>
2023-03-10 17:22:17 +0530

    ALL MESSAGE FUNCTIONS

--- src/Components/Admin.jsx
+++ src/Components/Admin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Table, Layout, Button } from "antd";
+import { Button,Table, Form, Input, Typography ,Layout} from "antd";
 import { Link} from "react-router-dom";
 import { Navbar } from "./Navbar";
 import { useNavigate} from "react-router-dom";
@@ -60,10 +60,13 @@ export const Admin = () => {
   const [teachers, setTeachers] = useState([]);
   const [admin, setAdmin] = useState([]);
   const [users, setUsers] = useState([]);
+  const[message,setmessage] = useState(null);
   const [selected, setSelected] = useState(null);
+  const [role,setRole] = useState(null);
   let tokenJson = JSON.parse(localStorage.getItem('login'));
   const navigate = useNavigate();
   let token = "Bearer "+ tokenJson.accessToken;
+  const [form] = Form.useForm();
 
   const handleRowClick = (result) => {
     navigate("/UserDetails",{state: {name:result.name,id:result.idNumber,birthday:result.dateOfBirth,address:result.address,
@@ -71,6 +74,21 @@ export const Admin = () => {
     }})
 
   };
+
+  const handleSubmit = () =>{
+    let token = "Bearer "+ tokenJson.accessToken;
+    let sid = tokenJson.id;
+    const name = role;
+    const messages = {sid,name,message}
+    form.resetFields();
+     fetch('http://localhost:8080/admin/putMessageByRole',{
+         method:"POST",
+         headers:{"Content-Type":"application/json",
+         "Authorization":token
+        },
+         body:JSON.stringify(messages)
+       })
+     }
   
 
 
@@ -83,6 +101,7 @@ export const Admin = () => {
       .then(res=>res.json())
       .then((result)=>{
       setStudents(result);
+      setRole("STUDENT")
       }
       )
     setSelected("students");
@@ -97,6 +116,7 @@ export const Admin = () => {
       .then(res=>res.json())
       .then((result)=>{
       setTeachers(result);
+      setRole("TEACHER")
       }
       )
     setSelected("teachers");
@@ -111,6 +131,7 @@ export const Admin = () => {
       .then(res=>res.json())
       .then((result)=>{
       setAdmin(result);
+      setRole("ADMIN")
       }
       )   
     setSelected("admin");
@@ -217,7 +238,20 @@ export const Admin = () => {
                 onClick: () => handleRowClick(record)
               };
             }}/>
-        </div>
+          </div>
+          <div>
+               <Form className="loginForm" form={form} onFinish={handleSubmit}>
+               <Typography.Title>Send Message</Typography.Title>
+                    <Form.Item rules={[{
+                        required:true,
+                        message:"please enter your username"
+                    }]}label="Username" name={"my username"}>
+                        <Input value = {message} onChange={(e) => setmessage(e.target.value)} placeholder="Enter your message"/>
+                    </Form.Item>
+                    <Button type="primary" htmlType="submit" block >Send</Button>
+                    <Button type="primary" onClick={()=>navigate("/Dashboard")}>Back</Button>
+               </Form>
+            </div>
       </Layout.Content>
     </Layout>
     </div>
